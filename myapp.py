55df7e93e7c42f08d78c4e608add0a802a05c2fc
lmemnem <vns328@alumni.ku.dk>
2024-06-05 14:12:02 +0200

    added some import

--- myapp.py
+++ myapp.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 import psycopg2
-
+import re
+import uuid
 
 #Columns
 #"id" VARCHAR(100)
@@ -34,23 +35,31 @@ def connection():
         port="5432")
     return curr_con
 
-
 @app.route('/')
+def homie():
+    return home()
+
+@app.route('/home')
 def home():
     return render_template('interface.html')
 
+
+#search and submits
 @app.route('/search', methods=['GET'])
 def search():
     conn = connection()
     cur = conn.cursor()
+    cur.execute("SELECT MIN(white_rating) AS min_white_rating, MIN(black_rating) AS min_black_rating FROM chess_games;")
+    results_min = cur.fetchone()
+    min_white, min_black = results_min
     cur.execute("SELECT MAX(white_rating) AS max_white_rating, MAX(black_rating) AS max_black_rating FROM chess_games;")
-    results = cur.fetchone()
-    max_white, max_black = results
+    results_max = cur.fetchone()
+    max_white, max_black = results_max
     cur.execute("SELECT DISTINCT opening_name FROM chess_games ORDER BY opening_name;")
     openings = cur.fetchall()
     cur.close()
     conn.close()
-    return render_template('results.html', max_white=max_white, max_black=max_black,openings=openings)
+    return render_template('results.html', min_white=min_white, min_black=min_black,max_white=max_white,max_black=max_black,openings=openings)
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -60,7 +69,6 @@ def submit():
 
     conn = connection()
     cur = conn.cursor()
-
     query = "SELECT id, winner, white_id, black_id, white_rating, black_rating, opening_name FROM chess_games WHERE 1=1"
     params = []
 
@@ -80,10 +88,110 @@ def submit():
     conn.close()
 
     return render_template('results.html', games=games)
+####
+
+
+#IMPORT AND IMPORTING
+
+
+def parser(data):
+    pattern = re.compile(r"\[([A-Za-z0-9]+)\s+\"(.+?)\"\]")
+    metadata = dict(pattern.findall(data))
+    stripped_moves = re.sub(r'(\{[^}]*\})|(\([^)]*\))', '', data.split('\n\n', 1)[1]).strip()
+    moves = re.sub(r'\d+\.', '', stripped_moves).replace("  ", " ").strip()
+
+    return metadata, moves
 
 
 
+#def prep_moves(moves):
+#    moves = moves.split()
+#    result = []
+#    for i in range(len(moves)):
+#        curr = []
+#        for j in range(i,len(moves)):
+#            if moves[i] = int 
+#    return result
+    
 
+def whowon(fields, moves):
+    result = ""
+    winner = ""
+    if '#' in moves:
+        result = "checkmate"
+    elif "draw" in moves:
+        result = "draw"
+    else:
+        result= "no winner?"
+    data = fields.get("Result", "")
+    if '1-0' in data:
+        winner = "white"
+    elif '0-1' in data:
+        winner = "black"
+    else:
+        winner = "draw"
+    return result, winner
+
+
+
+def prepare_for_db(fields, moves):
+    victory_stats, winner = whowon(fields,moves)
+    
+
+    game_data = {
+        "id": str(uuid.uuid4()),
+        "rated": "True", ##skal fixes
+        "created_at": fields.get("Date"),
+        "last_move_at": "something",
+        "turns": len(moves.split()), 
+        "victory_status": whowon(moves),
+        "winner": winner,
+        "increment_code": "unknown",
+        "white_id": fields.get("White", "unknown"),
+        "black_id": fields.get("Black", "unknown"),
+        "white_rating": fields.get("WhiteElo","unknown"),
+        "black_rating": fields.get("BlackElo","unknown"),
+        "moves": moves,
+        "opening_eco": fields.get("ECO", "unknown"), 
+        "opening_name": fields.get("Opening", "unknown"),
+        "opening_ply": "unknown"
+    }
+    return game_data
+
+def insert_game(game_data):
+    conn = connection()
+    cur = conn.cursor()
+
+    columns = ', '.join(game_data.keys())
+    placeholders = ', '.join(['%s'] * len(game_data))
+    sql = f"INSERT INTO chess_games ({columns}) VALUES ({placeholders})"
+    
+    cur.execute(sql, list(game_data.values()))
+    conn.commit()
+    cur.close()
+    conn.close()
+    
+    
+    
+@app.route('/import', methods=['GET'])
+def import_page():
+    return render_template('import.html')
+
+
+@app.route('/importing', methods=['POST'])
+def importing():
+    file = request.files.get('pgn_file')
+    if file and file.filename != '':
+        pgn_content = file.read().decode('utf-8')
+    else:
+        pgn_content = request.form.get('pgn_text', '')  
+
+    ## use functions
+
+    print("Success")
+    return redirect(url_for('import_page'))
+
+##
 
 @app.route('/about')
 def about():
